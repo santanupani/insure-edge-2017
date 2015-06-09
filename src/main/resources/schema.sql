9526d6a809053bda1faeead7cccb18b04f0e9a2c
BinodSethi <binod.sethi@reverside.co.za>
2015-06-09 11:20:22 +0200

    DDL ststement to hold questions of all product

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,3 +27,32 @@ create sequence product_id_seq start with 1 increment by 1 ;
 
 create trigger product_id_trigger before insert on product referencing new row as new for each row set new.id = next value for product_id_seq;
 
+create table quotequestion(
+    id integer not null, 
+    maxamount varchar(100) not null, 
+    totalvalue varchar(100) not null,
+    additionalpremium varchar(100) not null, 
+    professionalcarriers varchar(100) not null,
+    timeperweek varchar(100) not null,
+    location varchar(100) not null,
+    distanceinkms varchar(100) not null,
+    goodsdescription varchar(100) not null,
+    vaultdetails varchar(100) not null,
+    sabs varchar(100) not null,
+    goodstorage varchar(100) not null,
+    reenforcedconcrete varchar(100) not null,
+    seismicconductors varchar(100) not null,
+    cctvfootgae varchar(100) not null,
+    alarmresponse varchar(100) not null,
+    physicaladdresscode varchar(100) not null,
+    postaladdresscode varchar(100) not null,
+    insuredstatus varchar(100) not null,
+    insureddetails varchar(100) not null,
+    requiressasria varchar(100) not null,
+    constraint quotequestion_pk primary key (id)
+);
+
+create sequence quotequestion_id_seq start with 1 increment by 1 ;
+
+create trigger quotequestion_id_trigger before insert on quotequestion referencing new row as new for each row set new.id = next value for quotequestion_id_seq;
+
