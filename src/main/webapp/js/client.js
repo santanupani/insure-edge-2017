0541f523ec3df8780327887252d1ad663746b9a6
Manmay <manmay.e.mohanty@gmail.com>
2015-06-10 14:16:30 +0200

    Creating view for dynamic questionnaires form

--- src/main/webapp/js/client.js
+++ src/main/webapp/js/client.js
@@ -46,8 +46,79 @@ polygon.controller('productsCtrl', function ($scope, $rootScope, $http) {
 
 polygon.controller('questionnairesCtrl', function ($scope, $rootScope, $http, $routeParams) {
 	
-	console.log($routeParams['id']);
-	console.log($rootScope.products);
+	$scope.questionnaires = [];
+	
+	$scope.init = function(){
+		console.log($rootScope.products);
+		if($rootScope.products==undefined) {
+			$scope.getProducts();
+		} else {
+			$scope.product = $rootScope.products[$routeParams['id']-1];
+			$scope.getQuestionnaires($routeParams['id']);
+		}
+	};
+	
+	$scope.getProducts = function () {
+        console.log('get products');
+        $http({
+            url: '/api/products',
+            method: 'get'
+        }).success(function (data, status) {
+            if (status == 200) {
+                console.log('retrived successfully');
+                $rootScope.products = data;
+                $scope.init();
+            } else {
+                console.log('status:' + status);
+                $rootScope.error = "error status code : " + status;;
+            }
+        }).error(function (error) {
+            console.log(error);
+        	$rootScope.error = error;;
+                
+        });
+    };
+    
+    $scope.getQuestionnaires = function(productId){
+    	$scope.questionnaires = 
+    		 [{
+   			  "id": "0",
+   			  "question" : "What is your name ?",
+   			  "answerType" : "text"
+   			},
+   			{
+   			  "id": "1",
+   			  "question" : "What is your gender ?",
+   			  "answerType" : "select",
+   			  "answerValues" : ["Female", "Male"]
+   			},
+   			{
+   			  "id": "2",
+   			  "question" : "What is your age ?",
+   			  "answerType" : "number",
+   			  "dependsOn" : 1,
+   			  "onAnswer"  : "Male"
+   			},
+   			{
+   			  "id": "3",
+   			  "question" : "What is your salary ?",
+   			  "answerType" : "number",
+   			  "dependsOn" : 1,
+   			  "onAnswer" : "Female"
+   			},
+   			{
+   			    "id" : "4",
+   			    "question" : "Do you have any criminal record ?",
+   			    "answerType" : "checkbox"
+   			},
+   			{
+   			    "id" : "5",
+   			    "question" : "Please provide details of your crime",
+   			    "answerType" : "textarea",
+   			    "dependsOn" : 4,
+   			    "onAnswer" : "true"
+   			}];
+    };
 	
 
 });
