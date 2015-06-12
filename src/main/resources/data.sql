428d32e25a4f5995964ecf58f26b3f1b79a269d8
Manmay <manmay.e.mohanty@gmail.com>
2015-06-12 14:21:06 +0200

    Review data sql fpr questionnaires for product 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,30 +22,30 @@ insert into questionnaires(product_id, sequence_number, question, answer_type_id
 insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '6', 'Please provide details of your crime', 	5, 	5,     'true');*/
 
 
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '1', 'Maximum amount you wish to insure: ?',                                  2, 	 null,  null);
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '2', 'Is the above amount the Total Full Value of the goods being moved:',    3,      1,  null);
-insert into answer_values(questionnaire_id, answer_value) values(2, 'Y');
-insert into answer_values(questionnaire_id, answer_value) values(2, 'N');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '3', 'What is the Total Full Value of the goods being moved:?',               2,      2,   'N');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '4', 'For Additional Premium,Do you want first loss cover:?',                 3,   null,  null);
-insert into answer_values(questionnaire_id, answer_value) values(4, 'Y');
-insert into answer_values(questionnaire_id, answer_value) values(4, 'N');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '5', 'Do you use the service of a professional valuables carriers:?',         3,    null, null);
-insert into answer_values(questionnaire_id, answer_value) values(5, 'Y');
-insert into answer_values(questionnaire_id, answer_value) values(5, 'N');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '6', 'What is the name of the professional valuables carriers:?',             3,       5,  'Y');
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '1', 'What is the maximum amount you wish to insure ?',                      2, 	null, 	null);
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '2', 'Is the above amount the total full value of the goods being moved ?',  4,	null, 	null);
+/*insert into answer_values(questionnaire_id, answer_value) values(2, 'Y');*/
+/*insert into answer_values(questionnaire_id, answer_value) values(2, 'N'); */
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '3', 'What is the total full value of the goods being moved ?',              2,   2, 		'false');
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '4', 'For Additional Premium, Do you want first loss cover ?',               4,   null,	null);
+/*insert into answer_values(questionnaire_id, answer_value) values(4, 'Y');*/
+/*insert into answer_values(questionnaire_id, answer_value) values(4, 'N');*/
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '5', 'Do you use the service of a professional valuables carriers ?',        4,   null,	null);
+/*insert into answer_values(questionnaire_id, answer_value) values(5, 'Y');*/
+/*insert into answer_values(questionnaire_id, answer_value) values(5, 'N');*/
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '6', 'What is the name of the professional valuables carriers ?',            3,   5,	  	'true');
 insert into answer_values(questionnaire_id, answer_value) values(6, 'Protea Coin Service');
 insert into answer_values(questionnaire_id, answer_value) values(6, 'G4S Service');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '7', 'By whom and how are the valuables carried:?',                           5,       5,  'N');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '8', 'How many times per week are the valuables carried:',                    3,    null,  null);
-insert into answer_values(questionnaire_id, answer_value) values(8, '1');
-insert into answer_values(questionnaire_id, answer_value) values(8, '2');
-insert into answer_values(questionnaire_id, answer_value) values(8, '3');
-insert into answer_values(questionnaire_id, answer_value) values(8, '4');
-insert into answer_values(questionnaire_id, answer_value) values(8, '5');
-insert into answer_values(questionnaire_id, answer_value) values(8, '6');
-insert into answer_values(questionnaire_id, answer_value) values(8, '7');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '9', 'From where and to where are the valuables carried?',                    3,    null,  null);
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '7', 'By whom and how are the valuables carried ?',                           5,	5, 		'false');
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '8', 'How many times per week are the valuables carried ?',                   3,  null,  	null);
+insert into answer_values(questionnaire_id, answer_value) values(8, '1 time /week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '2 times/week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '3 times/week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '4 times/week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '5 times/week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '6 times/week');
+insert into answer_values(questionnaire_id, answer_value) values(8, '7 times/week');
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '9', 'From where and to where are the valuables carried ?',                    3,    null,  null);
 insert into answer_values(questionnaire_id, answer_value) values(9, 'Port Elizabeth to Bloemfontein');
 insert into answer_values(questionnaire_id, answer_value) values(9, 'Johannesburg to Durban');
 insert into answer_values(questionnaire_id, answer_value) values(9, 'Polokwane to Mbombela');
@@ -61,8 +61,8 @@ insert into answer_values(questionnaire_id, answer_value) values(10, '300km to 4
 insert into answer_values(questionnaire_id, answer_value) values(10, '400km to 500km');
 insert into answer_values(questionnaire_id, answer_value) values(10, '500km to 600km');
 insert into answer_values(questionnaire_id, answer_value) values(10, '600km to 700km');
-insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '11', 'Do you require SASRIA cover?'                     ,                    3,    null,  null);
-insert into answer_values(questionnaire_id, answer_value) values(11, 'Y');
-insert into answer_values(questionnaire_id, answer_value) values(11, 'N');
+insert into questionnaires(product_id, sequence_number, question, answer_type_id, depends_on, on_answer)  values('1', '11', 'Do you require SASRIA cover?'                     ,                    4,    null,  null);
+/*insert into answer_values(questionnaire_id, answer_value) values(11, 'Y');*/
+/*insert into answer_values(questionnaire_id, answer_value) values(11, 'N');*/
 
 
