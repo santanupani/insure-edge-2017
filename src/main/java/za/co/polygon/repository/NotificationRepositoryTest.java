ef6aa746785f10fb0b1644dcc327e1fc48c4e290
ThaboThulare <thabo.thulare@reverside.co.za>
2015-06-19 16:52:52 +0200

    Moved password to noficationRepository , added two properties and declared session as a class variable and used logger instead of system print

--- src/main/java/za/co/polygon/repository/NotificationRepositoryTest.java
+++ src/main/java/za/co/polygon/repository/NotificationRepositoryTest.java
@@ -1,5 +1,9 @@
 package za.co.polygon.repository;
 
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import za.co.polygon.domain.Notification;
 
 public class NotificationRepositoryTest {
@@ -7,12 +11,18 @@ public class NotificationRepositoryTest {
     public static void main(String[] args) {
 
         NotificationRepository notificationRepository = new NotificationRepository();
+
+        notificationRepository.host = "smtp.gmail.com";
+        notificationRepository.port = "587";
+      
+
         Notification notification = new Notification();
         String to = "teethulare@gmail.com";
         String subject = "Test";
-        String message = "A test message";
-
+        String message = "A test Email message";
+        String from = "thabothulare68@gmail.com";
         notification.setTo(to);
+        notification.setFrom(from);
         notification.setSubject(subject);
         notification.setMessage(message);
 
