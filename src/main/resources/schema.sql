3034b1ef7c24bcf59ac6dca4920bbfc002b57db8
lkonaite <lkonaite@gmail.com>
2015-09-16 08:17:27 +0200

    Schema rework

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -173,7 +173,7 @@ create table contact_details(
 );
 
 
-/* table : schedule_attachings */
+/* table : schedule_attachings 
 
 create table schedule_attachings(
     id integer auto_increment not null primary key,
@@ -241,15 +241,15 @@ create table underwriters(
 /* table : sub_agents */
 create table sub_agents(
     id integer auto_increment not null primary key, 
-    underwriter_id integer not null,
+    broker_id integer not null,
     name varchar(64) not null, 
     title varchar(45),
     job_description varchar(128),
-    constraint sub_agent_detail_fk1 foreign key(underwriter_id) references underwriters(id),
+    constraint sub_agent_detail_fk1 foreign key(broker_id) references brokers(id),
 );
 
 
-/* table : commissions */
+/* table : commissions 
 create table commissions(
     id integer auto_increment not null primary key,
     underwriter_id integer not null,
@@ -258,15 +258,14 @@ create table commissions(
     constraint commissions_details_fk1 foreign key(underwriter_id) references underwriters(id),
     constraint commissions_details_fk2 foreign key(broker_id) references brokers(id),
 );
+*/
+
 
 /* table : policy_masters */
-create table policy_details(
+create table client_policies(
     id integer auto_increment not null primary key,
-    reference_no int not null,
     policy_request_id varchar(60) not null,
-    policy_no integer not null,
     sub_agent_id integer not null,
-    broker_id integer not null,
     insurer_id integer not  null ,
     client_detail_id integer not  null ,
     policy_inception_date date not null,
@@ -278,17 +277,13 @@ create table policy_details(
     sasria_frequency varchar(30) not null,
     device varchar(30) not null,
     retroactive_date date not null,
-    approved boolean,
     collect_by_debit_order boolean,
     exclude_sasria boolean,
     underwriter_policy_fee decimal not null,
     broker_policy_fee decimal not null,
     notes varchar(256) not null,
-    constraint policy_masters_details_fk1 foreign key (broker_id) references brokers (id),
     constraint policy_masters_details_fk2 foreign key (sub_agent_id) references sub_agents (id),
     constraint policy_masters_details_fk3 foreign key(insurer_id) references insurers(id),
     constraint policy_masters_details_fk4 foreign key(policy_request_id) references policy_requests(id),
-    constraint policy_masters_details_fk5 foreign key(client_detail_id) references client_details(id)
-
-
+    constraint policy_masters_details_fk5 foreign key(client_detail_id) references client_details(id),
 );
