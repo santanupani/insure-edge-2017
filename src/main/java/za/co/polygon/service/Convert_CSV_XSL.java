501e5c3b848634d94dd889e3f512f9d15cf96acc
BinodSethi <binod.sethi@reverside.co.za>
2016-01-12 10:42:07 +0200

    Changes

--- src/main/java/za/co/polygon/service/Convert_CSV_XSL.java
+++ src/main/java/za/co/polygon/service/Convert_CSV_XSL.java
@@ -16,67 +16,67 @@ import org.apache.poi.ss.usermodel.Cell;
 
 public class Convert_CSV_XSL {
 
-	@SuppressWarnings("deprecation")
-	public static void main(String args[]) throws IOException
-	{ 
-		ArrayList<Object> arList=null;
-		ArrayList<Object> al=null;
-		String thisLine;  
-		FileInputStream fis = new FileInputStream(new File("src/main/java/polygonCIB.csv"));
-		DataInputStream myInput = new DataInputStream(fis);
-		int i=0;
-		arList = new ArrayList<Object>();
-		while ((thisLine = myInput.readLine()) != null)
-		{
-			al = new ArrayList<Object>();
-			String strar[] = thisLine.split(",");
-			for(int j=0;j<strar.length;j++)
-			{
-				al.add(strar[j]);
-			}
-			arList.add(al);
-			System.out.println();
-			i++;
-		} 
-
-		try
-		{
-			HSSFWorkbook hwb = new HSSFWorkbook();
-			HSSFSheet sheet = hwb.createSheet("new sheet");
-			for(int k=0;k<arList.size();k++)
-			{
-				ArrayList<Object> ardata = (ArrayList)arList.get(k);
-				HSSFRow row = sheet.createRow((short) 0+k);
-				for(int p=0;p<ardata.size();p++)
-				{
-					HSSFCell cell = row.createCell((short) p);
-					String data = ardata.get(p).toString();
-					if(data.startsWith("=")){
-						cell.setCellType(Cell.CELL_TYPE_STRING);
-						data=data.replaceAll("\"", "");
-						data=data.replaceAll("=", "");
-						cell.setCellValue(data);
-					}else if(data.startsWith("\"")){
-						data=data.replaceAll("\"", "");
-						cell.setCellType(Cell.CELL_TYPE_STRING);
-						cell.setCellValue(data);
-					}else{
-						data=data.replaceAll("\"", "");
-						cell.setCellType(Cell.CELL_TYPE_NUMERIC);
-						cell.setCellValue(data);
-					}
-				}
-				System.out.println();
-			} 
-			FileOutputStream fileOut = new FileOutputStream("test.xls");
-			hwb.write(fileOut);
-			fileOut.close();
-			System.out.println("Your excel file has been generated");
-		} catch ( Exception ex ) {
-			ex.printStackTrace();
-		} //main method ends
-		finally{
-			myInput.close();
-		}
-	} 
+//	@SuppressWarnings("deprecation")
+//	public static void main(String args[]) throws IOException
+//	{ 
+//		ArrayList<Object> arList=null;
+//		ArrayList<Object> al=null;
+//		String thisLine;  
+//		FileInputStream fis = new FileInputStream(new File("src/main/java/polygonCIB.csv"));
+//		DataInputStream myInput = new DataInputStream(fis);
+//		int i=0;
+//		arList = new ArrayList<Object>();
+//		while ((thisLine = myInput.readLine()) != null)
+//		{
+//			al = new ArrayList<Object>();
+//			String strar[] = thisLine.split(",");
+//			for(int j=0;j<strar.length;j++)
+//			{
+//				al.add(strar[j]);
+//			}
+//			arList.add(al);
+//			System.out.println();
+//			i++;
+//		} 
+//
+//		try
+//		{
+//			HSSFWorkbook hwb = new HSSFWorkbook();
+//			HSSFSheet sheet = hwb.createSheet("new sheet");
+//			for(int k=0;k<arList.size();k++)
+//			{
+//				ArrayList<Object> ardata = (ArrayList)arList.get(k);
+//				HSSFRow row = sheet.createRow((short) 0+k);
+//				for(int p=0;p<ardata.size();p++)
+//				{
+//					HSSFCell cell = row.createCell((short) p);
+//					String data = ardata.get(p).toString();
+//					if(data.startsWith("=")){
+//						cell.setCellType(Cell.CELL_TYPE_STRING);
+//						data=data.replaceAll("\"", "");
+//						data=data.replaceAll("=", "");
+//						cell.setCellValue(data);
+//					}else if(data.startsWith("\"")){
+//						data=data.replaceAll("\"", "");
+//						cell.setCellType(Cell.CELL_TYPE_STRING);
+//						cell.setCellValue(data);
+//					}else{
+//						data=data.replaceAll("\"", "");
+//						cell.setCellType(Cell.CELL_TYPE_NUMERIC);
+//						cell.setCellValue(data);
+//					}
+//				}
+//				System.out.println();
+//			} 
+//			FileOutputStream fileOut = new FileOutputStream("test.xls");
+//			hwb.write(fileOut);
+//			fileOut.close();
+//			System.out.println("Your excel file has been generated");
+//		} catch ( Exception ex ) {
+//			ex.printStackTrace();
+//		} //main method ends
+//		finally{
+//			myInput.close();
+//		}
+//	} 
 }
